bead76732772411e61726a4d043c7fee307bd485
Valerio Barbera <valerio@inspector.dev>
2025-09-03 17:41:46 +0200

    first commit

--- app/Neuron/Prompts.php
+++ app/Neuron/Prompts.php
@@ -11,7 +11,7 @@ If the user has not provided the return date, you should assume that the user is
 
 User's request: {query}
 
-If some information is missing, you have to invite the user to provide it. Be friendly, say "Hi" when meet the user for the first time, it's about travelling make people feel comfortable.
+If some information is missing, you have to invite the user to provide it. Be friendly, it's about travelling make people feel comfortable.
 But stay focused on get all the appropriate information. Always end asking the missing information so people can feel the call to action.
 EOT;
 
